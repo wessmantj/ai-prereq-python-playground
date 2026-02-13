36ee0f328cadaca3b55583e7015bce2f88b6a16b
Travis Wessman <traviswessman26@gmail.com>
2026-02-13 13:11:44 -0500

    public push

---
+++ leetcode_patterns/top_k_elements.py
@@ -0,0 +1,38 @@
+# TOP K ELEMENTS ALGORITHM
+from typing import List, Optional, Tuple
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+def findKthLargest(nums: List[int], k: int) -> int:
+    '''
+    Given an integer array nums and an integer k, return the kth largest element in the array.
+
+    Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+    Can you solve it without sorting?
+    '''
+    
+
+n = [3,2,3,1,2,4,5,5,6]
+k = 4
+print(findKthLargest(n, k))
+
+"""
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    '''
+    Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+    '''
+    pass
+n = [1,2,1,2,1,2,3,1,3,2]
+k = 2
+
+def mergeKLists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    '''
+    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+    Merge all the linked-lists into one sorted linked-list and return it.
+    '''
+    pass
+"""
